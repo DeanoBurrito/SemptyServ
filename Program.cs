5227569accad702b4f83b66ecd7bdf95e556f341
Dean Tuckey <dean243@hotmail.com>
2020-08-20 09:39:26 +1000

    Repo init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SemptyServ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
